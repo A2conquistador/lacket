79005b028b057e9fe6b4cc647b0f0865dd76a5c7
A2conquistador <owenleroy1234@gmail.com>
2026-03-10 16:42:11 -0400

    fix config path

--- backend/handlers/configuration.js
+++ backend/handlers/configuration.js
@@ -1,3 +1,3 @@
 import fs from "fs";
 
-export default () => global.config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+export default () => global.config = JSON.parse(fs.readFileSync(new URL("../config.json", import.meta.url).pathname, "utf8"));
